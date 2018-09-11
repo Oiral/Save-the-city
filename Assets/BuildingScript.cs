835eecb355c891ff755e1915b0b608882c3ff5d1
Hamish <hamish@pageland.co.nz>
2018-09-11 21:59:21 +1200

    Added in building info storing

---
+++ Assets/BuildingScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingScript : MonoBehaviour {
+
+    public bool onFire;
+
+    public string nameOfPerson;
+    public float timeForFire;
+
+    public GameObject infoDisplay;
+    public GameObject infoDisplayPrefab;
+
+    public int fireEngines = 0;
+
+    private void Update()
+    {
+        //DisplayCheck();
+        
+        if (fireEngines > 0)
+        {
+            timeForFire -= Time.deltaTime * fireEngines;
+            Debug.Log(timeForFire);
+        }
+
+    }
+
+    public void DisplayCheck()
+    {
+        //if the building is on fire and there isn't a display
+        if (onFire && infoDisplay == null)
+        {
+            infoDisplay = Instantiate(infoDisplayPrefab, transform.position, Quaternion.identity, transform);
+        }
+
+        //if the building is not on fire and there is a display
+        if (!onFire && infoDisplay != null)
+        {
+            Destroy(infoDisplay);
+            //make sure infoDisplay is cleared
+            infoDisplay = null;
+        }
+    }
+
+
+    public void FireEngineNear()
+    {
+        fireEngines += 1;
+    }
+}
--- Assets/FireManagerScript.cs
+++ Assets/FireManagerScript.cs
@@ -27,25 +27,40 @@ public class FireManagerScript : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //StartFire();
-            GetName();
+            StartFire();
+            //GetName();
         }
     }
 
     public void StartFire()
     {
         GameObject randomBuilding = buildings[Random.Range(0, buildings.Count)];
+        BuildingScript buildingScript = randomBuilding.GetComponent<BuildingScript>();
+        while (buildingScript.onFire)
+        {
+            randomBuilding = buildings[Random.Range(0, buildings.Count)];
+            buildingScript = randomBuilding.GetComponent<BuildingScript>();
+        }
 
         Instantiate(fireParticlePrefab, randomBuilding.transform.position, Quaternion.Euler(-90, 0, 0));
+
+        buildingScript.fireEngines = 0;
+        buildingScript.onFire = true;
+        buildingScript.timeForFire = Random.Range(5, 11);
+        buildingScript.nameOfPerson = GetName();
+        Debug.Log(buildingScript.timeForFire);
+        
+        
     }
 
-    public void GetName()
+    public string GetName()
     {
         List<string> firstNames = ReadAndSplitFile("Assets/Resources/Names.txt" , '\n');
 
         List<string> lastNames = ReadAndSplitFile("Assets/Resources/LastNames.txt", '\n');
 
         Debug.Log(firstNames[Random.Range(0, firstNames.Count)] +" "+ lastNames[Random.Range(0, lastNames.Count)]);
+        return (firstNames[Random.Range(0, firstNames.Count)] + " " + lastNames[Random.Range(0, lastNames.Count)]);
     }
 
     public List<string> ReadAndSplitFile(string path,char splitString)
