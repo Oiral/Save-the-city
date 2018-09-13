21b2a27a53f0022173c7efc44373e9f805299165
Unknown <hamish@pageland.co.nz>
2018-09-14 11:36:42 +1200

    Adding in some interaction stuff

---
+++ Assets/Interaction/AnswersScriptable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Action { LoseTeam, GainMoney, LoseMoney };
+
+[CreateAssetMenu(fileName = "Answer", menuName = "Interaction/Answer", order = 1)]
+public class AnswersScriptable : ScriptableObject {
+    [Multiline]
+    public string description;
+    public Action actionToTake;
+    public int actionNumber;
+
+    [Multiline]
+    public string chosenDescription;
+
+
+    public string GetChosenDesc()
+    {
+        string text = chosenDescription;
+
+        return text.Replace("{num}", actionNumber.ToString());
+    }
+}
