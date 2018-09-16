b75d724cbe42b68ea9622efcb5348fd58cb75b95
Unknown <hamish@pageland.co.nz>
2018-09-17 11:52:57 +1200

    Reading CSV file

---
+++ Assets/ReadCSV.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCSV : MonoBehaviour{
+
+    public TextAsset csvFile;
+
+    static public string[,] SplitGrid(string csvText)
+    {
+        string[] lines = csvText.Split("\n"[0]);
+
+        // finds the max width of row
+        int width = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] row = SplitCsvLine(lines[i]);
+            width = Mathf.Max(width, row.Length);
+        }
+
+        // creates new 2D string grid to output to
+        string[,] outputGrid = new string[width + 1, lines.Length + 1];
+        for (int y = 0; y < lines.Length; y++)
+        {
+            string[] row = SplitCsvLine(lines[y]);
+            for (int x = 0; x < row.Length; x++)
+            {
+                outputGrid[x, y] = row[x];
+
+                // This line was to replace "" with " in my output. 
+                // Include or edit it as you wish.
+                outputGrid[x, y] = outputGrid[x, y].Replace("\"\"", "\"");
+            }
+        }
+
+        return outputGrid;
+    }
+
+    static public string[] SplitCsvLine(string line)
+    {
+        return line.Split(',');
+    }
+
+    static public void DebugArray(string[,] array)
+    {
+        string textOutput = "";
+        for (int y = 0; y < array.GetUpperBound(1); y++)
+        {
+            for (int x = 0; x < array.GetUpperBound(0); x++)
+            {
+
+                textOutput += array[x, y];
+                textOutput += "|";
+            }
+            textOutput += "\n";
+        }
+        Debug.Log(textOutput);
+    }
+
+    
+}
---
+++ Assets/SpawnGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGrid : MonoBehaviour {
+    public GameObject boxPrefab;
+
+    public TextAsset file;
+
+	// Use this for initialization
+	void Start () {
+        string[,] grid = ReadCSV.SplitGrid(file.text);
+
+        for (int x = 0; x < grid.GetUpperBound(0); x++)
+        {
+            for (int z = 0; z < grid.GetUpperBound(1); z++)
+            {
+                int y = int.Parse(grid[x, z]);
+
+                GameObject objectInstance = Instantiate(boxPrefab);
+                objectInstance.transform.position = new Vector3(x, y, z);
+            }
+        }
+	}
+}
