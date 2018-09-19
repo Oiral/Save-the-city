a85c7143436c9a1dd2a54e750cdc31f53756e17d
Unknown <hamish@pageland.co.nz>
2018-09-20 11:50:49 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Squad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SquadType {General,Fire,Rescue,Demolishion };
+
+public class Squad {
+
+    public SquadType typeOfSquad = SquadType.General;
+
+    public bool onMission = false;
+
+    public int exp;
+
+    public int level = 0;
+}
