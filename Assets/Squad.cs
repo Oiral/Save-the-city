d9d6a6d0dc2d19d1f5044cc96a34964af7a66833
Unknown <hamish@pageland.co.nz>
2018-09-21 11:46:50 +1200

    Added in squad manager and squads

--- Assets/Squad.cs
+++ Assets/Squad.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum SquadType {General,Fire,Rescue,Demolishion };
 
-public class Squad {
+public class Squad : MonoBehaviour{
 
     public SquadType typeOfSquad = SquadType.General;
 
@@ -13,4 +14,55 @@ public class Squad {
     public int exp;
 
     public int level = 0;
+
+    public GameObject disableObject;
+    public Text squadName;
+
+    Button squadButton;
+
+    ColorBlock baseColour;
+    public ColorBlock selectedBlockColours;
+
+    public bool selected;
+
+    SquadManager manager;
+
+    private void Start()
+    {
+        squadButton = GetComponent<Button>();
+        baseColour = squadButton.colors;
+        manager = transform.parent.GetComponent<SquadManager>();
+
+        squadName.text = "Squad " + manager.squads.Count;
+    }
+
+    public void SquadSelected()
+    {
+        //Deselect every other button attached to parent
+        foreach (Squad sqaud in manager.squads)
+        {
+            sqaud.SquadDeselect();
+        }
+
+        manager.activeSquad = this;
+
+        squadButton.colors = selectedBlockColours;
+
+        selected = true;
+        
+    }
+
+    public void SquadDeselect()
+    {
+        squadButton.colors = baseColour;
+        selected = false;
+    }
+
+    public void GoToMission()
+    {
+        onMission = true;
+
+        disableObject.SetActive(onMission);
+        squadButton.interactable = !onMission;
+    }
 }
---
+++ Assets/SquadManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadManager : MonoBehaviour {
+    
+    public GameObject SquadUIPrefab;
+
+    public List<Squad> squads;
+
+    public Squad activeSquad;
+
+    public void NewSquad()
+    {
+        GameObject SquadUI = Instantiate(SquadUIPrefab, transform);
+        squads.Add(SquadUI.GetComponent<Squad>());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            NewSquad();
+        }
+    }
+}
